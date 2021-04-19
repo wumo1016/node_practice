5d8dbee5668234aca07462278364f6bcddf1f5c4
wumo1016 <760478279@qq.com>
2021-04-19 21:10:34 +0800

    update

--- src/basic/1.js
+++ src/basic/1.js
@@ -21,16 +21,49 @@
 //  - cwd(current working direction) 当前工作目录，运行目录
 //  - env 环境变量(用户变量+系统变量)
 //  - argv (两个参数 1:执行node所在的文件 2.当前执行的问价 ..后面就是用户执行命令时传入的参数)
-//  - nextTick
+//  - nextTick node自己实现的 优先级比promise高
 //  - chdir 改变工作目录
 
-// 测试命令 node 1.js --port 3000 --file xxx
-const program = require('commander')
-program.option('-p, --port <n>', 'set user port') // 简称 全称 值 提示信息
-program.option('-f, --file <n>', 'set user directory')
-program.parse(program.env)
-console.log(program.opts()); // { port: '3000', file: 'xxx' } 参数需要先配置
+// 8.EventLoop
+/*
+本阶段执行已经被 setTimeout() 和 setInterval() 的调度回调函数。
+   ┌───────────────────────────┐
+┌─>│           timers          │
+│  └─────────────┬─────────────┘
+|   执行延迟到下一个循环迭代的 I/O 回调。
+│  ┌─────────────┴─────────────┐
+│  │     pending callbacks     │
+│  └─────────────┬─────────────┘
+|   仅系统内部使用。
+│  ┌─────────────┴─────────────┐
+│  │       idle, prepare       │
+│  └─────────────┬─────────────┘
+|  检索新的I/O事件;执行与 I/O相关的回调┌───────────────┐
+│  ┌─────────────┴─────────────┐      │   incoming:   │
+│  │           poll            │<─────┤  connections, │
+│  └─────────────┬─────────────┘      │   data, etc.  │
+│  setImmediate() 回调函数在这里执行。 └───────────────┘
+│  ┌─────────────┴─────────────┐
+│  │           check           │
+│  └─────────────┬─────────────┘
+|  一些关闭的回调函数
+│  ┌─────────────┴─────────────┐
+└──┤      close callbacks      │
+   └───────────────────────────┘
+*/
 
+const fs = require('fs')
+fs.readFile('README.md', 'utf8', () => {
+  setTimeout(() => {
+    console.log('setTimeout');
+  })
+  setImmediate(() => {
+    console.log('setImmediate');
+  })
+})
+// setImmediate
+// setTimeout
+// 文件读取属于poll阶段
 
 
 /* env相关
@@ -61,4 +94,40 @@ console.log(process.argv.slice(2).reduce((memo, current, index, arr) => {
 }, {}));
 
 一般使用库 commander args等
+// 测试命令 node 1.js --port 3000 --file xxx
+const program = require('commander')
+program.option('-p, --port <n>', 'set user port') // 简称 全称 值 提示信息
+program.option('-f, --file <n>', 'set user directory')
+program.parse(program.env)
+console.log(program.opts()); // { port: '3000', file: 'xxx' } 参数需要先配置
+*/
+
+/* nextTick
+Promise.resolve().then(_ => {
+  console.log('then');
+})
+process.nextTick(() => {
+  console.log('nextTick');
+})
+// nextTick
+// then
+*/
+
+
+/* EventLoop
+
+// 示例1：
+// 有可能setImmediate先走 因为setTimeout计时器受进程性能的影响
+setTimeout(() => {
+  console.log('setTimeout');
+})
+setImmediate(() => {
+  console.log('setImmediate');
+})
+// setTimeout
+// setImmediate
+
+// 示例2：
+
+
 */
