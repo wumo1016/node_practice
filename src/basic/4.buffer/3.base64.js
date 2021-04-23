6f766e23125e786fca43ea96df132019f8b0b0f5
wumo1016 <760478279@qq.com>
2021-04-23 22:19:18 +0800

    update(base64)

--- src/basic/4.buffer/3.base64.js
+++ src/basic/4.buffer/3.base64.js
@@ -4,15 +4,10 @@
 const r = Buffer.from('吴').toString('base64')
 console.log(r); // 5ZC0 四个字节 原来是三个字节
 
-// const b1 = Buffer.from('5ZC0')
-// console.log(b1); // <Buffer 35 5a 43 30>
-// console.log(0x35.toString(2), 0x5a.toString(2), 0x43.toString(2), 0x30.toString(2));
-
 // base64就是将每个字节转化成 小于64的值
 const b1 = Buffer.from('吴')
 console.log(b1); // <Buffer e5 90 b4>
 console.log(0xe5.toString(2), 0x90.toString(2), 0xb4.toString(2));
-
 // 11100101 10010000 10110100 3 * 8 => 4 * 6
 // 为什么拆为最大6位 因为就是要小于64
 // 111001 011001 000010 110100 => 转换为10进制 57 25 2 52
@@ -21,4 +16,4 @@ console.log(parseInt('111001', 2), parseInt('011001', 2), parseInt('000010', 2),
 let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 str += str.toLocaleLowerCase()
 str += '0123456789+/'
-console.log(str[57] + str[25] + str[2] + str[52]);
+console.log(str[57] + str[25] + str[2] + str[52]); // 5ZC0
