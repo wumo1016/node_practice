4a6ecd9205737f78c860c0e3f67443bda3356f0a
wumo1016 <760478279@qq.com>
2021-04-25 17:04:48 +0800

    update(buffer)

--- src/basic/4.buffer/4.fs.js
+++ src/basic/4.buffer/4.fs.js
@@ -13,7 +13,9 @@
   - 读文件
     - 不写编码 默认就是buffer
     - 如果文件不存在 就报错
-  - 写文件 
+  - 写文件
+    - 如果文件已经存在 就会先清空 再写入
+    - 如果没有就先创建
     - 默认以utf8格式写入 会将数据调用 toString() 然后再写入
 */
 
@@ -29,21 +31,60 @@ const path = require('path')
 // })
 
 /* open write */
-// r代表读 w代表都 a代表追加
+// r代表读 w代表都 a代表追加 r+以读取为准可以写入操作 w+以写入为准可以读取操作
 // fd file descriptor 是一个number类型
 
-let buf = Buffer.alloc(3)
-fs.open(path.resolve(__dirname, './a.txt'), 'r', function (err, fd) {
-  fs.read(fd, buf, 0, 3, 6, function(err, rnumber){ // rnumber 读取到的真实个数
-    // console.log(rnumber);
-    fs.open(path.resolve(__dirname, './b.txt'), 'w', function(err, wfd){
-      fs.write(wfd, buf, 0, 3, 0, function(err, wnumber){
-        console.log(wnumber);
-        fs.close(fd, () => {}) // 最终读完之后 要关闭文件
-        fs.close(wfd, () => {})
-      })
+// let buf = Buffer.alloc(3)
+// fs.open(path.resolve(__dirname, './a.txt'), 'r', function (err, fd) {
+//   fs.read(fd, buf, 0, 3, 6, function(err, rnumber){ // rnumber 读取到的真实个数
+//     // console.log(rnumber);
+//     fs.open(path.resolve(__dirname, './b.txt'), 'w', function(err, wfd){
+//       fs.write(wfd, buf, 0, 3, 0, function(err, wnumber){
+//         console.log(wnumber);
+//         fs.close(fd, () => {}) // 最终读完之后 要关闭文件
+//         fs.close(wfd, () => {})
+//       })
+//     })
+//   })
+// })
+
+/* copy a.txt到b.txt 每次复制3个字节 */
+function copy(source, target, cb) {
+  let bufSize = 3
+  let buf = Buffer.alloc(bufSize)
+  let r_offset = 0
+  let w_offset = 0
+  fs.open(path.resolve(__dirname, source), 'r', function (err, rfd) {
+    if (err) return cb(err)
+    fs.open(path.resolve(__dirname, target), 'w', function (err, wfd) {
+      if (err) return cb(err)
+      function next() {
+        fs.read(rfd, buf, 0, bufSize, r_offset, function (err, rnumber) {
+          if (err) return cb(err)
+          if (rnumber) {
+            fs.write(wfd, buf, 0, rnumber, w_offset, function (err, wnumber) {
+              r_offset += rnumber
+              w_offset += wnumber
+              next()
+            })
+          } else {
+            cb()
+            fs.close(rfd, () => {})
+            fs.close(wfd, () => {})
+          }
+        })
+      }
+      next()
     })
   })
+}
+
+copy('./a.txt', './b.txt', function (err) {
+  if (err) return console.log(err);
+  console.log('拷贝成功');
 })
+
+
+
 
 // 
