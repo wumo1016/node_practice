a764e70683a59e1cb98ee4d5ca425f4807b81208
wumo1016 <760478279@qq.com>
2021-05-12 16:13:58 +0800

    feat(文件上传)

--- src/basic/11.http-header/1.server.js
+++ src/basic/11.http-header/1.server.js
@@ -3,8 +3,24 @@ const url = require('url')
 const path = require('path')
 const fs = require('fs')
 const querystring = require('querystring')
-
+// 第三方模块
 const mime = require('mime')
+const uuid = require('uuid');
+
+const uploadPath = path.resolve(__dirname, 'upload');
+
+Buffer.prototype.split = function (sep) {
+  let len = Buffer.from(sep).length
+  let offset = 0,
+    current;
+  let list = []
+  while (-1 !== (current = this.indexOf(sep, offset))) {
+    list.push(this.slice(offset, current))
+    offset = current + len
+  }
+  list.push(this.slice(offset));
+  return list
+}
 
 const server = http.createServer((req, res) => {
   let {
@@ -45,8 +61,32 @@ const server = http.createServer((req, res) => {
       } else if (ctype === 'application/x-www-form-urlencoded') {
         let obj = querystring.parse(buf.toString(), '&', '=') // 默认对应的数据格式 a=1&b=2
         res.end(JSON.stringify(obj))
+      } else if (ctype.startsWith('multipart/form-data')) {
+        let boundary = '--' + ctype.split('=')[1]
+        let lines = buf.split(boundary).slice(1, -1); // 前后两段是不需要的
+        const r = {};
+        lines.forEach(line => {
+          // 把header 和 内容进行拆分
+          let [head, body] = line.split(`\r\n\r\n`); // head和body中间两个换行两个个回车
+          head = head.toString();
+          let name = head.match(/name="(.+?)"/)[1];
+          if (head.includes('filename')) {
+            // 文件上传  将文件内容上传到服务器的上传文件夹中
+            let buffer = line.slice(head.length + 4, -2);
+            let filename = uuid.v4();
+            // 你最终创建完名字之后 还会同步到数据库里
+            // 下次查找数据库，再找到对应的文件名
+            fs.writeFileSync(path.join(uploadPath, filename), buffer)
+            r[name] = {
+              filename,
+              size: buffer.length
+            }
+          } else {
+            r[name] = body.toString().slice(0, -2)
+          }
+        })
+        res.end(JSON.stringify(r));
       } else {
-        console.log(buf.toString());
         res.end('ok')
       }
     })
@@ -56,10 +96,9 @@ const server = http.createServer((req, res) => {
       if (err) {
         return sendError(res)
       }
-
       if (stateObj.isFile()) {
-        res.setHeader('Content-Type', mime.getType(requestFile) + ';charset=utf-8')
-        fs.createReadStream(requestFile).pipe(res)
+        res.setHeader('Content-Type', mime.getType(filePath) + ';charset=utf-8')
+        fs.createReadStream(filePath).pipe(res)
       } else {
         let htmlPath = path.join(filePath, 'index.html')
         fs.access(htmlPath, function (err) {
@@ -79,7 +118,6 @@ function sendError(res) {
   res.statusCode = 404
   res.end('NOT FOUND')
 }
-
 
 server.listen(3000, function () {
   console.log('server start');
