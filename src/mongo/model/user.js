7b3856d3aafb4cd18508bca66b669db3e8daf7b9
wumo1016 <760478279@qq.com>
2021-05-18 09:12:33 +0800

    feat(mongo3)

--- src/mongo/model/user.js
+++ src/mongo/model/user.js
@@ -1,12 +1,8 @@
 const mongoose = require('./index')
 
-const UserSchema = new mongoose.Schema({
+module.exports = mongoose.model('User', new mongoose.Schema({
   username: String,
   password: String,
   gender: Number,
   age: Number
-})
-
-const UserModel = mongoose.model('User', UserSchema, 'user')
-
-module.exports = UserModel
+}), 'user')
